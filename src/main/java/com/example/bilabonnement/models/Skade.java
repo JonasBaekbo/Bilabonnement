096cf4274b314592ee56b4047dbcecf8a003452d
adambl99 <adam6925@stud.kea.dk>
2022-05-09 10:14:10 +0200

    Constructor tilføjet til "skade"

--- src/main/java/com/example/bilabonnement/models/Skade.java
+++ src/main/java/com/example/bilabonnement/models/Skade.java
@@ -1,4 +1,29 @@
 package com.example.bilabonnement.models;
 
 public class Skade {
+
+    private String skadetype;
+    private int pris;
+
+    public Skade(String skadetype, int pris) {
+        this.skadetype = skadetype;
+        this.pris = pris;
+    }
+
+    public String getSkadetype() {
+        return skadetype;
+    }
+
+    public void setSkadetype(String skadetype) {
+        this.skadetype = skadetype;
+    }
+
+    public int getPris() {
+        return pris;
+    }
+
+    public void setPris(int pris) {
+        this.pris = pris;
+    }
 }
+
