c1630c0998fefa7bee2ef62d9ec52d64524bbd81
Bruger <mikk00j3@stud.kea.dk>
2022-05-18 11:28:33 +0200

    start på ekstraEquipemnt

---
+++ src/main/java/com/example/bilabonnement/models/ekstraEquipemnt.java
@@ -0,0 +1,30 @@
+package com.example.bilabonnement.models;
+
+import java.util.ArrayList;
+
+public class ekstraEquipemnt {
+
+    private Car car;
+    private ArrayList<String> ekstraEquipemntdescription;
+
+    public ekstraEquipemnt (Car car, ArrayList<String> ekstraEquipemntdescription){
+        this.car = car;
+        this.ekstraEquipemntdescription = ekstraEquipemntdescription;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public ArrayList<String> getEkstraEquipemntdescription() {
+        return ekstraEquipemntdescription;
+    }
+
+    public void setEkstraEquipemntdescription(ArrayList<String> ekstraEquipemntdescription) {
+        this.ekstraEquipemntdescription = ekstraEquipemntdescription;
+    }
+}
